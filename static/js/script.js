0020a4bde1459e893d24534ece87b29f4fc08672
talhabinalam <talhabinalam98@gmail.com>
2023-09-24 12:46:00 +0600

    code commit

---
+++ static/js/script.js
@@ -0,0 +1,53 @@
+$('.navToggle').on('click', function (e) {
+  e.preventDefault();
+  $('body').toggleClass('navToggleActive');
+});
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+    $('body').addClass('fixedHeader');
+  } else {
+    $('body').removeClass('fixedHeader');
+  }
+});
+
+
+var swiper = new Swiper(".quoteSwiper", {
+  navigation: {
+    nextEl: ".test-swiper-button-next",
+    prevEl: ".test-swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+
+
+var swiper = new Swiper(".certificatesSlider", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".cert-swiper-button-next",
+    prevEl: ".cert-swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+  },
+});
